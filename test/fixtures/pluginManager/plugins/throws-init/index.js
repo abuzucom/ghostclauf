17529f38ac51c9215bdb353cabd517910ffe22b3
itsjustatank <654939+itsjustatank@users.noreply.github.com>
2026-08-20 10:21:21 -0500

    fix: rollback failed plugin initialization

--- test/fixtures/pluginManager/plugins/throws-init/index.js
+++ test/fixtures/pluginManager/plugins/throws-init/index.js
@@ -1,7 +1,19 @@
 export default {
   name: 'fixture-throws-init',
   version: '1.0.0',
-  init() {
+  init(ctx) {
+    ctx.command({
+      trigger: 'partial-command',
+      allow: ['everyone'],
+      handler() {},
+    });
+    ctx.on('chatMessage', () => {
+      ctx.command({
+        trigger: 'partial-listener-command',
+        allow: ['everyone'],
+        handler() {},
+      });
+    });
     throw new Error('init boom');
   },
 };
